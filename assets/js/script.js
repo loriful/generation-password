3cb382c2fd0802edfbc54b18b5f7ddac9d362a8f
loriful <tiptonica@gmail.com>
2022-01-09 17:16:54 -0500

    cleaned comments, character selection, and removed debug statements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
-//
 
+/* **************************************************************************************** */
 // function to gather and validate password length
 var pwLength = function() {
 
@@ -9,7 +9,7 @@ var pwLength = function() {
   
   // get password length, check for integer, iterate until valid input
   while (Number.isNaN(len - parseInt(len)) || len < 8 || len > 128) {
-
+  
     // prompt user for pw length between 8 and 128, bash away until valid input received
     len = window.prompt("Welcome to the Password Generator. Please provide password character length as a whole number between 8 and 128.");  
 
@@ -27,7 +27,8 @@ var pwLength = function() {
 
 }; // end function pwlength
 
-// get password character types
+/* **************************************************************************************** */
+// get password character types, input a string for promt, return boolean for user preferance
 var getCharType = function(charType) {
   
   var getType = window.confirm("Should your password include " + charType + " ?");
@@ -35,6 +36,7 @@ var getCharType = function(charType) {
   return getType;
 }; // end getCharType
 
+/* **************************************************************************************** */
 // generate random numbers
 var randomNumber = function(min, max) {
   var value = Math.floor(Math.random() * (max - min) + min);
@@ -42,6 +44,7 @@ var randomNumber = function(min, max) {
   return value;
 }; // end function randomNumber
 
+/* **************************************************************************************** */
 // build password from options
 var generatePassword = function() {
 
@@ -52,50 +55,42 @@ var generatePassword = function() {
     num: getCharType("numbers"),
     sym: getCharType("symbols")
   }
-  
-  // keep track of selection of character types
-  var charOptions = 0;
+
   // store potential character types for random selection
   var charArray = [];
-  
+  // prove valid selection
+  var charOptions = 0;
+  // the randomized characters
+  var pwArray = [];
+
+  // build an array of requested character types based on response
   if (pwRulesObj.upper) {
-    // build UPPER case array
-    charArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-    // at least one selection made
-    charOptions += 1;
+      charArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+      charOptions += 1;
   }
   if (pwRulesObj.lower) {
-    // build LOWER case array
-    charArray.push('a','b','c','d','e','f','g','h','i','j','k','l','m','p','q','r','s','t','u','v','w','x','y','z');
-    // at least one selection made
-    charOptions += 1;
+      charArray.push('a','b','c','d','e','f','g','h','i','j','k','l','m','p','q','r','s','t','u','v','w','x','y','z');
+      charOptions += 1;
   }
   if (pwRulesObj.num) {
-    // build number array
-    charArray.push('0','1','2','3','4','5','6','7','8','9');
-    // at least one selection made
-    charOptions += 1;
+      charArray.push('0','1','2','3','4','5','6','7','8','9');
+      charOptions += 1;
   }
   if (pwRulesObj.sym) {
-    charArray.push('!','@','#','$','%','&','*','+');
-    // at least one selection made
+    charArray.push('!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','`','{','|','}','~');  
     charOptions += 1;
   }
-  // No selections made, force entry
-  if (charOptions === 0) {
+  if (charOptions === 0) {  // No selections made, force entry
     window.alert("You have not selected a character type.  All lower case has been selected for you.");
     var charArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m','p','q','r','s','t','u','v','w','x','y','z'];
   }
-  
-  // hold the randomized characters
-  var pwArray = [];
-  
-  // build a randomized array
+    
+  // build a randomized password from allowed rules
   for (var i = 0; i < pwRulesObj.length; i++) {
     pwArray[i] = charArray[(randomNumber(0,charArray.length-1))];
   } 
 
-  // send back a concatenated array
+  // send back a clean password string
   return pwArray.join("");
 
 }; // end function generatePassword
